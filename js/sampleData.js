884988dfa2747c83b5d32613d7488a40dd0f6485
Heitor Chang <heitorchang@gmail.com>
2016-03-25 19:35:58 -0300

    account tables

---
+++ js/sampleData.js
@@ -0,0 +1,65 @@
+// sample data
+
+// accountType may be: bens, despesas, obrigacoes, receitas, patrimonio
+var sampleAccounts = [
+  {
+    id: 0,
+    accountType: "receitas",
+    name: "Salary",
+    balance: 2000,
+  },
+
+  {
+    id: 1,
+    accountType: "bens",
+    name: "Bank",
+    balance: 3000,
+  },
+
+  {
+    id: 2,
+    accountType: "bens",
+    name: "Wallet",
+    balance: 120,
+  },
+
+  {
+    id: 3,
+    accountType: "despesas",
+    name: "Entertainment",
+    balance: 623,
+  },
+
+  {
+    id: 4,
+    accountType: "despesas",
+    name: "Groceries",
+    balance: 540,
+  },
+  
+  
+
+];
+
+var sampleTransactions = [
+  {
+    id: 0,
+    debit: 3,
+    credit: 2,
+    amount: 25,
+    description: "Movies",
+    date: "2016-03-20 19:20",
+  },
+
+  {
+    id: 1,
+    debit: 1,
+    credit: 5,
+    amount: 2500,
+    description: "Feb. salary",
+    date: "2016-03-02 12:10",
+  },
+
+  
+];
+
