dbf08539b4ad054265d5ecfbf37ce422cdc2b95a
Heitor Chang <heitorchang@gmail.com>
2016-03-31 13:26:16 -0300

    dummy add transaction

--- js/sampleData.js
+++ js/sampleData.js
@@ -12,66 +12,120 @@ var sampleAccounts = [
   {
     id: 1,
     accountType: "bens",
-    name: "Bank",
-    balance: 3000,
+    name: "Conta corrente",
+    balance: 565,
   },
 
   {
     id: 2,
     accountType: "bens",
-    name: "Wallet",
+    name: "Carteira",
     balance: 120,
   },
 
   {
     id: 3,
     accountType: "despesas",
-    name: "Entertainment",
+    name: "Diversão",
     balance: 623,
   },
 
   {
     id: 4,
     accountType: "despesas",
-    name: "Groceries",
+    name: "Supermercado",
     balance: 540,
   },
   
   {
     id: 5,
     accountType: "obrigacoes",
-    name: "Credit card",
+    name: "Cartão de crédito",
     balance: 100,
   },
   
   {
     id: 6,
     accountType: "patrimonio",
-    name: "Opening equity",
+    name: "Saldo de abertura",
     balance: 3400,
   },
   
-  
+  {
+    id: 7,
+    accountType: "bens",
+    name: "Poupança",
+    balance: 1240,
+  },
+
+  {
+    id: 8,
+    accountType: "bens",
+    name: "Investimentos",
+    balance: 3212,
+  },
+
+  {
+    id: 9,
+    accountType: "despesas",
+    name: "Luz, água e gás",
+    balance: 192,
+  },
+
+  {
+    id: 10,
+    accountType: "despesas",
+    name: "Restaurantes",
+    balance: 321,
+  },
+
+  {
+    id: 11,
+    accountType: "receitas",
+    name: "Juros",
+    balance: 23,
+  },
+
+  {
+    id: 12,
+    accountType: "despesas",
+    name: "Tarifas",
+    balance: 39,
+  },
+
+  {
+    id: 13,
+    accountType: "despesas",
+    name: "Roupas",
+    balance: 63,
+  },
+
+  {
+    id: 14,
+    accountType: "despesas",
+    name: "Cabeleireiros",
+    balance: 90,
+  },
 
 ];
 
 var sampleTransactions = [
   {
     id: 0,
+    date: "2016-03-20 19:20",
+    description: "Cinema",
+    amount: 25,
     debit: 3,
     credit: 2,
-    amount: 25,
-    description: "Movies",
-    date: "2016-03-20 19:20",
   },
 
   {
     id: 1,
+    date: "2016-03-02 12:10",
+    description: "Salário de fev. 2016",
+    amount: 1500,
     debit: 1,
     credit: 0,
-    amount: 2500,
-    description: "Feb. salary",
-    date: "2016-03-02 12:10",
   },
 
   
